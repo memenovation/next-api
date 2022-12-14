9c4963dc3dabbed2d37c84a21eb81751d8f98ff5
Logan Tai <logantai24@gmail.com>
2022-12-14 14:41:50 +0000

    chore: refactor example code

--- src/pages/api/v1/example/inactive.ts
+++ src/pages/api/v1/example/inactive.ts
@@ -1,32 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import NextCors from "nextjs-cors";
 
 //functions
 import { apiHandler } from "@functions/api/APIHandler";
 
-//types
-import { UpdatedNextApiRequest } from "@Types/index";
-
 //api handler to wrap all endpoints
-export default apiHandler(handler);
+export default apiHandler(handler, ["GET"]);
 
 /*
   This route is not defined as active in src/configs/ActiveEndpoints.ts, 
   so when calling this endpoint, it will throw an error and return a 500 error.
 */
-async function handler(req: UpdatedNextApiRequest, res: NextApiResponse) {
-  await NextCors(req, res, {
-    // Options
-    methods: ["GET", "HEAD", "PUT", "POST", "OPTIONS"],
-    origin: "*",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
-
-  /* ------------------- check if request method is correct ------------------- */
-  if (req.method !== "GET") {
-    res.status(400).json({ error: "Invalid Request" });
-    return;
-  }
-
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ name: "Inactive" });
 }
